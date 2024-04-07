a0813314c660e8bd24e26955a3114c52a471867b
Sung-Jin Ahn <sahn0307@gmail.com>
2024-04-07 15:05:27 -0600

    add delete route

--- server/app.py
+++ server/app.py
@@ -36,7 +36,7 @@ class Plants(Resource):
         db.session.commit()
 
         return make_response(new_plant.to_dict(), 201)
-
+    
 
 api.add_resource(Plants, '/plants')
 
@@ -47,6 +47,22 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        data = request.get_json()
+        plant = db.session.get(Plant, id)
+
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = db.session.get(Plant, id)
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
